0c3dedd295d57841153bac615fa93990f231cda6
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-07-10 19:24:37 +0000

    agragregando.com

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ let action = ["love", "eat", "enjoy"];
 
 function generarNombresDominio(pronombres, adjetivos, sustantivos) {
   let nombresDominio = [];
-  let conteo=0
- pronoun.forEach((pronoun) => {
-    adj.forEach((adj) => {
-      noun.forEach((noun) => {
-        action.forEach((action) => {
-          let nombreDominio = pronoun + adj + noun + action;
+  let conteo = 0;
+  pronoun.forEach(pronoun => {
+    adj.forEach(adj => {
+      noun.forEach(noun => {
+        action.forEach(action => {
+          let nombreDominio = pronoun + adj + noun + action + ".com";
           nombresDominio.push(nombreDominio);
-          conteo ++;
+          conteo++;
         });
       });
     });
